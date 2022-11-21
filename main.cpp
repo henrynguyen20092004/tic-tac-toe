49ebdb77b9d108db904755ff53f0552c029652bf
henrynguyen20092004 <nguyenvinhhien20092004@gmail.com>
2022-11-21 08:32:35 +0700

    init main

--- main.cpp
+++ main.cpp
@@ -4,6 +4,27 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world";
+    char ans;
+    bool running;
+    cout << "Welcome to the game tic-tac-toe." << endl;
+    cout << "Do you want to start? (Y/N) ";
+    cin >> ans;
+    do
+    {
+        while (ans != 'Y' && ans != 'y' && ans != 'N' && ans != 'n')
+        {
+            cout << "Please try again! ";
+            cin >> ans;
+        }
+        if (ans == 'Y' || ans == 'y')
+        {
+            running = true;
+            cout << "Do sth here!" << endl;
+            cout << "Do you want to play again? (Y/N) ";
+            cin >> ans;
+        }
+        if (ans == 'N' || ans == 'n')
+            running = false;
+    } while (running);
     return 0;
 }
